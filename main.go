45d1fec14f6a77f7abcb48d233f934a44c14aecc
Marouane Bouaricha <marouane@itchallenge.fr>
2023-11-06 17:03:56 +0100

    Added concurrent jobs

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"sync"
 	"time"
 
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore"
@@ -29,6 +30,7 @@ const (
 	diskName          = "happybit-disk"
 	publicIPName      = "happybit-public-ip"
 	location          = "francecentral"
+	desiredIP         = "52.143.143.195"
 )
 
 var (
@@ -52,17 +54,30 @@ func main() {
 	if len(subscriptionId) == 0 {
 		log.Fatal("AZURE_SUBSCRIPTION_ID is not set.")
 	}
-	//create virtual machine
-	createVM()
+	fmt.Printf("Running jobs...")
+	numJobs := 10
 
-	keepResource := os.Getenv("KEEP_RESOURCE")
-	if len(keepResource) == 0 {
-		//delete virtual machine
-		cleanup()
+	var wg sync.WaitGroup
+	resultChan := make(chan string, numJobs)
+
+	for i := 0; i < numJobs; i++ {
+		wg.Add(1)
+		go createVM(&wg, i, resultChan)
 	}
+
+	go func() {
+		wg.Wait()
+		close(resultChan)
+	}()
+
+	for result := range resultChan {
+		fmt.Println(result)
+	}
+
 }
 
-func createVM() {
+func createVM(wg *sync.WaitGroup, jobID int, resultChan chan string) {
+	defer wg.Done()
 	conn, err := connectionAzure()
 	if err != nil {
 		log.Fatalf("cannot connect to Azure:%+v", err)
@@ -90,32 +105,32 @@ func createVM() {
 	disksClient = computeClientFactory.NewDisksClient()
 
 	log.Println("start creating virtual machine...")
-	virtualNetwork, err := createVirtualNetwork(ctx)
+	virtualNetwork, err := createVirtualNetwork(ctx, jobID)
 	if err != nil {
 		log.Fatalf("cannot create virtual network:%+v", err)
 	}
 	log.Printf("Created virtual network: %s", *virtualNetwork.ID)
 
-	subnet, err := createSubnets(ctx)
+	subnet, err := createSubnets(ctx, jobID)
 	if err != nil {
 		log.Fatalf("cannot create subnet:%+v", err)
 	}
 	log.Printf("Created subnet: %s", *subnet.ID)
 
-	publicIP, err := createPublicIP(ctx)
+	publicIP, err := createPublicIP(ctx, jobID)
 	if err != nil {
 		log.Fatalf("cannot create public IP address:%+v", err)
 	}
 	log.Printf("Created public IP address: %s", *publicIP.ID)
 
-	netWorkInterface, err := createNetWorkInterface(ctx, *subnet.ID, *publicIP.ID)
+	netWorkInterface, err := createNetWorkInterface(ctx, *subnet.ID, *publicIP.ID, jobID)
 	if err != nil {
 		log.Fatalf("cannot create network interface:%+v", err)
 	}
 	log.Printf("Created network interface: %s", *netWorkInterface.ID)
 
 	networkInterfaceID := netWorkInterface.ID
-	virtualMachine, err := createVirtualMachine(ctx, *networkInterfaceID)
+	virtualMachine, err := createVirtualMachine(ctx, *networkInterfaceID, jobID)
 	if err != nil {
 		log.Fatalf("cannot create virual machine:%+v", err)
 	}
@@ -124,44 +139,51 @@ func createVM() {
 	log.Println("Virtual machine created successfully")
 	fmt.Println("Waiting for Public IP address to be allocated...")
 	time.Sleep(10 * time.Second)
-	getPublicIP()
+	allocatedIP := getPublicIP(jobID)
+	if allocatedIP == desiredIP {
+		resultChan <- fmt.Sprintf("Job %d: Allocated IP address matches the desired IP address: %s", jobID, allocatedIP)
+	}
+	if allocatedIP != desiredIP {
+		resultChan <- fmt.Sprintf("Job %d: Allocated IP address (%s) does not match the desired IP address (%s).", jobID, allocatedIP, desiredIP)
+		cleanup(jobID)
+	}
 }
 
-func cleanup() {
+func cleanup(jobID int) {
 	ctx := context.Background()
 
 	log.Println("start deleting virtual machine...")
-	err := deleteVirtualMachine(ctx)
+	err := deleteVirtualMachine(ctx, jobID)
 	if err != nil {
 		log.Fatalf("cannot delete virtual machine:%+v", err)
 	}
 	log.Println("deleted virtual machine")
 
-	err = deleteDisk(ctx)
+	err = deleteDisk(ctx, jobID)
 	if err != nil {
 		log.Fatalf("cannot delete disk:%+v", err)
 	}
 	log.Println("deleted disk")
 
-	err = deleteNetWorkInterface(ctx)
+	err = deleteNetWorkInterface(ctx, jobID)
 	if err != nil {
 		log.Fatalf("cannot delete network interface:%+v", err)
 	}
 	log.Println("deleted network interface")
 
-	err = deletePublicIP(ctx)
+	err = deletePublicIP(ctx, jobID)
 	if err != nil {
 		log.Fatalf("cannot delete public IP address:%+v", err)
 	}
 	log.Println("deleted public IP address")
 
-	err = deleteSubnets(ctx)
+	err = deleteSubnets(ctx, jobID)
 	if err != nil {
 		log.Fatalf("cannot delete subnet:%+v", err)
 	}
 	log.Println("deleted subnet")
 
-	err = deleteVirtualNetWork(ctx)
+	err = deleteVirtualNetWork(ctx, jobID)
 	if err != nil {
 		log.Fatalf("cannot delete virtual network:%+v", err)
 	}
@@ -170,13 +192,13 @@ func cleanup() {
 	log.Println("success deleted virtual machine.")
 }
 
-func getPublicIP() {
-	resp, err := publicIPAddressesClient.Get(context.Background(), resourceGroupName, publicIPName, nil)
+func getPublicIP(x int) string {
+	resp, err := publicIPAddressesClient.Get(context.Background(), resourceGroupName, fmt.Sprintf("%s-%d", publicIPName, x), nil)
 	if err != nil {
 		log.Fatalf("failed to get public IP address: %v", err)
 	}
 	ipAddress := *resp.PublicIPAddress.Properties.IPAddress
-	fmt.Println("Public IP Address: ", ipAddress)
+	return ipAddress
 }
 func connectionAzure() (azcore.TokenCredential, error) {
 	cred, err := azidentity.NewDefaultAzureCredential(nil)
@@ -186,28 +208,20 @@ func connectionAzure() (azcore.TokenCredential, error) {
 	return cred, nil
 }
 
-func createVirtualNetwork(ctx context.Context) (*armnetwork.VirtualNetwork, error) {
+func createVirtualNetwork(ctx context.Context, x int) (*armnetwork.VirtualNetwork, error) {
 
 	parameters := armnetwork.VirtualNetwork{
 		Location: to.Ptr(location),
 		Properties: &armnetwork.VirtualNetworkPropertiesFormat{
 			AddressSpace: &armnetwork.AddressSpace{
 				AddressPrefixes: []*string{
-					to.Ptr("10.1.0.0/16"), // example 10.1.0.0/16
+					to.Ptr("10.1.0.0/16"),
 				},
 			},
-			//Subnets: []*armnetwork.Subnet{
-			//	{
-			//		Name: to.Ptr(subnetName+"3"),
-			//		Properties: &armnetwork.SubnetPropertiesFormat{
-			//			AddressPrefix: to.Ptr("10.1.0.0/24"),
-			//		},
-			//	},
-			//},
 		},
 	}
 
-	pollerResponse, err := virtualNetworksClient.BeginCreateOrUpdate(ctx, resourceGroupName, vnetName, parameters, nil)
+	pollerResponse, err := virtualNetworksClient.BeginCreateOrUpdate(ctx, resourceGroupName, fmt.Sprintf("%s-%d", vnetName, x), parameters, nil)
 	if err != nil {
 		return nil, err
 	}
@@ -220,9 +234,9 @@ func createVirtualNetwork(ctx context.Context) (*armnetwork.VirtualNetwork, erro
 	return &resp.VirtualNetwork, nil
 }
 
-func deleteVirtualNetWork(ctx context.Context) error {
+func deleteVirtualNetWork(ctx context.Context, x int) error {
 
-	pollerResponse, err := virtualNetworksClient.BeginDelete(ctx, resourceGroupName, vnetName, nil)
+	pollerResponse, err := virtualNetworksClient.BeginDelete(ctx, resourceGroupName, fmt.Sprintf("%s-%d", vnetName, x), nil)
 	if err != nil {
 		return err
 	}
@@ -235,7 +249,7 @@ func deleteVirtualNetWork(ctx context.Context) error {
 	return nil
 }
 
-func createSubnets(ctx context.Context) (*armnetwork.Subnet, error) {
+func createSubnets(ctx context.Context, x int) (*armnetwork.Subnet, error) {
 
 	parameters := armnetwork.Subnet{
 		Properties: &armnetwork.SubnetPropertiesFormat{
@@ -243,7 +257,7 @@ func createSubnets(ctx context.Context) (*armnetwork.Subnet, error) {
 		},
 	}
 
-	pollerResponse, err := subnetsClient.BeginCreateOrUpdate(ctx, resourceGroupName, vnetName, subnetName, parameters, nil)
+	pollerResponse, err := subnetsClient.BeginCreateOrUpdate(ctx, resourceGroupName, fmt.Sprintf("%s-%d", vnetName, x), fmt.Sprintf("%s-%d", subnetName, x), parameters, nil)
 	if err != nil {
 		return nil, err
 	}
@@ -256,9 +270,9 @@ func createSubnets(ctx context.Context) (*armnetwork.Subnet, error) {
 	return &resp.Subnet, nil
 }
 
-func deleteSubnets(ctx context.Context) error {
+func deleteSubnets(ctx context.Context, x int) error {
 
-	pollerResponse, err := subnetsClient.BeginDelete(ctx, resourceGroupName, vnetName, subnetName, nil)
+	pollerResponse, err := subnetsClient.BeginDelete(ctx, resourceGroupName, fmt.Sprintf("%s-%d", vnetName, x), subnetName, nil)
 	if err != nil {
 		return err
 	}
@@ -271,7 +285,7 @@ func deleteSubnets(ctx context.Context) error {
 	return nil
 }
 
-func createPublicIP(ctx context.Context) (*armnetwork.PublicIPAddress, error) {
+func createPublicIP(ctx context.Context, x int) (*armnetwork.PublicIPAddress, error) {
 
 	parameters := armnetwork.PublicIPAddress{
 		Location: to.Ptr(location),
@@ -280,7 +294,7 @@ func createPublicIP(ctx context.Context) (*armnetwork.PublicIPAddress, error) {
 		},
 	}
 
-	pollerResponse, err := publicIPAddressesClient.BeginCreateOrUpdate(ctx, resourceGroupName, publicIPName, parameters, nil)
+	pollerResponse, err := publicIPAddressesClient.BeginCreateOrUpdate(ctx, resourceGroupName, fmt.Sprintf("%s-%d", publicIPName, x), parameters, nil)
 	if err != nil {
 		return nil, err
 	}
@@ -292,9 +306,9 @@ func createPublicIP(ctx context.Context) (*armnetwork.PublicIPAddress, error) {
 	return &resp.PublicIPAddress, err
 }
 
-func deletePublicIP(ctx context.Context) error {
+func deletePublicIP(ctx context.Context, x int) error {
 
-	pollerResponse, err := publicIPAddressesClient.BeginDelete(ctx, resourceGroupName, publicIPName, nil)
+	pollerResponse, err := publicIPAddressesClient.BeginDelete(ctx, resourceGroupName, fmt.Sprintf("%s-%d", publicIPName, x), nil)
 	if err != nil {
 		return err
 	}
@@ -306,7 +320,7 @@ func deletePublicIP(ctx context.Context) error {
 	return nil
 }
 
-func createNetWorkInterface(ctx context.Context, subnetID string, publicIPID string) (*armnetwork.Interface, error) {
+func createNetWorkInterface(ctx context.Context, subnetID string, publicIPID string, x int) (*armnetwork.Interface, error) {
 
 	parameters := armnetwork.Interface{
 		Location: to.Ptr(location),
@@ -329,7 +343,7 @@ func createNetWorkInterface(ctx context.Context, subnetID string, publicIPID str
 		},
 	}
 
-	pollerResponse, err := interfacesClient.BeginCreateOrUpdate(ctx, resourceGroupName, nicName, parameters, nil)
+	pollerResponse, err := interfacesClient.BeginCreateOrUpdate(ctx, resourceGroupName, fmt.Sprintf("%s-%d", nicName, x), parameters, nil)
 	if err != nil {
 		return nil, err
 	}
@@ -342,9 +356,9 @@ func createNetWorkInterface(ctx context.Context, subnetID string, publicIPID str
 	return &resp.Interface, err
 }
 
-func deleteNetWorkInterface(ctx context.Context) error {
+func deleteNetWorkInterface(ctx context.Context, x int) error {
 
-	pollerResponse, err := interfacesClient.BeginDelete(ctx, resourceGroupName, nicName, nil)
+	pollerResponse, err := interfacesClient.BeginDelete(ctx, resourceGroupName, fmt.Sprintf("%s-%d", nicName, x), nil)
 	if err != nil {
 		return err
 	}
@@ -357,7 +371,7 @@ func deleteNetWorkInterface(ctx context.Context) error {
 	return nil
 }
 
-func createVirtualMachine(ctx context.Context, networkInterfaceID string) (*armcompute.VirtualMachine, error) {
+func createVirtualMachine(ctx context.Context, networkInterfaceID string, x int) (*armcompute.VirtualMachine, error) {
 	//require ssh key for authentication on linux
 	// sshPublicKeyPath := "~/.ssh/id_rsa.pub"
 	// var sshBytes []byte
@@ -390,7 +404,7 @@ func createVirtualMachine(ctx context.Context, networkInterfaceID string) (*armc
 					Version:   to.Ptr("latest"),
 				},
 				OSDisk: &armcompute.OSDisk{
-					Name:         to.Ptr(diskName),
+					Name:         to.Ptr(fmt.Sprintf("%s-%s", diskName, string(x))),
 					CreateOption: to.Ptr(armcompute.DiskCreateOptionTypesFromImage),
 					Caching:      to.Ptr(armcompute.CachingTypesReadWrite),
 					ManagedDisk: &armcompute.ManagedDiskParameters{
@@ -403,7 +417,7 @@ func createVirtualMachine(ctx context.Context, networkInterfaceID string) (*armc
 				VMSize: to.Ptr(armcompute.VirtualMachineSizeTypes("Standard_B1ls")), // VM size include vCPUs,RAM,Data Disks,Temp storage.
 			},
 			OSProfile: &armcompute.OSProfile{ //
-				ComputerName:  to.Ptr("happybit"),
+				ComputerName:  to.Ptr(fmt.Sprintf("%s-%s", vmName, string(x))),
 				AdminUsername: to.Ptr("azureuser"),
 				AdminPassword: to.Ptr("Password01!@#"),
 				//require ssh key for authentication on linux
@@ -429,7 +443,7 @@ func createVirtualMachine(ctx context.Context, networkInterfaceID string) (*armc
 		},
 	}
 
-	pollerResponse, err := virtualMachinesClient.BeginCreateOrUpdate(ctx, resourceGroupName, vmName, parameters, nil)
+	pollerResponse, err := virtualMachinesClient.BeginCreateOrUpdate(ctx, resourceGroupName, fmt.Sprintf("%s-%s", vmName, string(x)), parameters, nil)
 	if err != nil {
 		return nil, err
 	}
@@ -442,9 +456,9 @@ func createVirtualMachine(ctx context.Context, networkInterfaceID string) (*armc
 	return &resp.VirtualMachine, nil
 }
 
-func deleteVirtualMachine(ctx context.Context) error {
+func deleteVirtualMachine(ctx context.Context, x int) error {
 
-	pollerResponse, err := virtualMachinesClient.BeginDelete(ctx, resourceGroupName, vmName, nil)
+	pollerResponse, err := virtualMachinesClient.BeginDelete(ctx, resourceGroupName, fmt.Sprintf("%s-%d", vmName, x), nil)
 	if err != nil {
 		return err
 	}
@@ -457,9 +471,9 @@ func deleteVirtualMachine(ctx context.Context) error {
 	return nil
 }
 
-func deleteDisk(ctx context.Context) error {
+func deleteDisk(ctx context.Context, x int) error {
 
-	pollerResponse, err := disksClient.BeginDelete(ctx, resourceGroupName, diskName, nil)
+	pollerResponse, err := disksClient.BeginDelete(ctx, resourceGroupName, fmt.Sprintf("%s-%d", diskName, x), nil)
 	if err != nil {
 		return err
 	}
